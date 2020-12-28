edb9a032fb20883a35c693b4cb7ad2f9eabc5508
Szqii <szqii.altn@gmail.com>
2020-12-28 23:16:29 +0300

    Add Toast plugin 🍕

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import VueToast from 'vue-toast-notification';
+// Import one of the available themes
+//import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-sugar.css';
+Vue.config.productionTip = false;
+
+Vue.use(VueToast);
+
+
 
 new Vue({
   render: h => h(App),
